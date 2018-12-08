89ef543f1b446d544f08dd04de45d6b9b0cf824f
Ulisses Ferreira <ulissesvf@gmail.com>
2018-12-08 03:19:51 +0000

    feat: add a link to home when 404

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class ErrorPage extends PureComponent {
 
@@ -6,6 +7,7 @@ export default class ErrorPage extends PureComponent {
     return (
       <div>
         <h1>Oops that page doesn't exist!</h1>
+        <Link to='/'>Go home</Link>
       </div>
     )
   }
