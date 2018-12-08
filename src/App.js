cde812277ccfcbc34efc908306657404b05911ee
Ulisses Ferreira <ulissesvf@gmail.com>
2018-12-08 03:11:22 +0000

    feat: simple apply page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import APIservice from './services/api.js';
+
 import './App.css';
 
 import Main from './components/Main';
 import Subscribe from './components/Subscribe';
 import Apply from './components/Apply';
 import Companies from './components/Companies';
+import ErrorPage from './components/ErrorPage';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      company: window.location.pathname.split('/')[1],
+      position: window.location.pathname.split('/')[2],
+      currentInternship: {
+        fields: {
+          Company: 'Loading',
+          Title: 'Loading'
+        }
+      }
+    }
+  }
+
+  getCurrentJob = (records) => {
+    for (let i = 0; i < records.length; i++) {
+      if (records[i].fields.Company.toLowerCase() === this.state.company && this.state.position === records[i].fields.Title.toLowerCase().replace(/ /g, '-')) {
+        return records[i]
+      }
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.company !== 'subscribe' && this.state.company !== 'companies') {
+      APIservice.getAllOffers().then(data => {
+        this.setState({
+          currentInternship: this.getCurrentJob(data.records)
+        })
+      })
+    }
+  }
+
+  Apply = () => {
+    return (
+      <Apply currentInternship={this.state.currentInternship} />
+    )
+  }
+
   render() {
+    console.log(window.location.href)
     return (
       <Router>
         <div className="app">
@@ -17,7 +60,8 @@ export default class App extends Component {
             <Route exact path='/' component={Main} />
             <Route exact path='/subscribe' component={Subscribe} />
             <Route exact path='/companies' component={Companies} />
-            <Route path='/:company/:position' component={Apply} />
+            <Route exact path='/404' component={ErrorPage} />
+            <Route path='/:company/:position' component={this.Apply} />
           </Switch>
         </div>
       </Router>
