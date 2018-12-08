caeced574bc5f6802396246883f993058e6b9e92
Ulisses Ferreira <ulissesvf@gmail.com>
2018-12-08 19:44:19 +0000

    feat: fetch Airtable information and display it

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -1,19 +1,27 @@
 import React, { PureComponent } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router-dom';
 import APIservice from '../services/api.js';
 
-export default class Apply extends PureComponent {
+class Apply extends PureComponent {
 
   constructor(props) {
     super(props)
     this.state = {
       company: window.location.pathname.split('/')[1],
       position: window.location.pathname.split('/')[2],
-      currentInternship: {
-        fields: {
-          Company: 'Loading',
-          Title: 'Loading'
-        }
+      currentInternship: { //These are all the fields we need. Before we get them we display a placeholder
+        Company: 'Loading',
+        Title: 'Loading',
+        Location: 'Loading',
+        AmbassadorBio: 'Loading',
+        AmbassadorPic: [{
+          thumbnails: {
+            large: 'Loading'
+          }
+        }],
+        AmbassadorTwitter: 'Loading',
+        MainRequirements: 'Loading',
+        JobDescription: 'Loading'
       }
     }
   }
@@ -22,7 +30,7 @@ export default class Apply extends PureComponent {
     for (let i = 0; i < records.length; i++) {
       try {
         if (records[i].fields.Company.toLowerCase().replace(/ /g, '-') === this.state.company && this.state.position === records[i].fields.Title.toLowerCase().replace(/ /g, '-')) {
-          return records[i]
+          return records[i].fields
         }
       } catch {
         //Something is undefined, just next
@@ -42,6 +50,10 @@ export default class Apply extends PureComponent {
     alert('Applying')
   }
 
+  goHome = () => {
+    this.props.history.push('/');
+  }
+
   render() {
     if (this.state.currentInternship === undefined) {
       return (
@@ -52,7 +64,7 @@ export default class Apply extends PureComponent {
         <div>
           <nav>
             <div className="wrapper">
-              <img className="logo" src="/media/logo.svg" alt="Upframe"></img>
+              <img className="logo" src="/media/logo.svg" alt="Upframe" onClick={this.goHome}/>
               <ul>
                 <li>
                   Internships
@@ -62,31 +74,32 @@ export default class Apply extends PureComponent {
           </nav>
           <div className="header-smaller">
             <div>
-              <h1>{this.state.currentInternship.fields.Title}</h1>
+              <h1>{this.state.currentInternship.Title} {this.state.currentInternship.Location}</h1>
             </div>
           </div>
           <div className="container">
-            INTERNSHIPS > {this.state.currentInternship.fields.Company.toUpperCase()} > {this.state.currentInternship.fields.Title.toUpperCase()}
+            INTERNSHIPS  >  {this.state.currentInternship.Company.toUpperCase()}  >  {this.state.currentInternship.Title.toUpperCase()}
             <div>
               <h1>
                 Job description
               </h1>
               <div>
-                Tenho que ir buscar ao airtable
+                {this.state.currentInternship.JobDescription}
               </div>
               <h1>
                 Main requirements
               </h1>
               <div>
-                tenho que ir buscar ao airtable
+                {this.state.currentInternship.MainRequirements}
               </div>
               <button onClick={this.apply}>Apply now</button>
+              {/* Ambassador Begin */}
               <div>
-                Photo do Malik
-                <p>
-                  At Upframe we believe anyone can make a difference. I'm a Co-founder at Upframe
-                </p>
+                <img src={this.state.currentInternship.AmbassadorPic[0].thumbnails.large.url} alt='Ambassador'></img>
+                <p>{this.state.currentInternship.AmbassadorBio}</p>
+                <a href={'https://www.twitter.com/' + this.state.currentInternship.AmbassadorTwitter}>{this.state.currentInternship.AmbassadorTwitter}</a>
               </div>
+              {/* End of Ambassador */}
             </div>
           </div>
         </div>
@@ -94,3 +107,5 @@ export default class Apply extends PureComponent {
     }
   }
 }
+
+export default withRouter(Apply)
