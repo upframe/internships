8010e755e54ab786cc4fffbd511917e1307ab97a
Ulisses Ferreira <ulissesvf@gmail.com>
2018-12-08 03:11:07 +0000

    feat: simple 404 page

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+export default class ErrorPage extends PureComponent {
+
+  render() {
+    return (
+      <div>
+        <h1>Oops that page doesn't exist!</h1>
+      </div>
+    )
+  }
+}
