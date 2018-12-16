ea71b85cbf841285a6e2c97238d1a311d9ea3942
Fábio Ferreira <ffcfpten@gmail.com>
2018-12-16 17:01:18 +0000

    feat: hamburger menu navbar with smooth animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function hamburgerClick() {
+  let navbar = document.querySelector('nav'),
+    hamburger = document.querySelector('.hamburger')
+
+  if(hamburger.classList.contains('open')) {
+    hamburger.classList.remove('open')
+    navbar.classList.remove('menu-open')
+  }
+  else {
+    hamburger.classList.add('open')
+    navbar.classList.add('menu-open')
+  }
+}
+
+function navbarScroll() {
+  document.addEventListener('scroll', (ev) => {
+    let nav = document.querySelector('nav')
+    if(window.scrollY && !nav.classList.contains('active')) {
+      nav.classList.add('active')
+    } else if(!window.scrollY && nav.classList.contains('active')) {
+      nav.classList.remove('active')
+    }
+  })
+}
+
 function Navbar(props) {
+  navbarScroll()
+  
   return (
     <nav>
       <div className="wrapper">
         <Link to="/" className="logo">
           <img src="/media/logo.svg" alt="Upframe"></img>
         </Link>
+
+        <div className="hamburger" onClick={hamburgerClick}>
+        </div>
+
         <ul>
-          <li>
-            Internships
-          </li>
+          <a href="."><li>People</li></a>
+          <a href="/"><li>Internships</li></a>
+          <a href="."><li>Contact</li></a>
         </ul>
       </div>
     </nav>
